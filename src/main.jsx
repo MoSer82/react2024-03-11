908403e713a5059c598fdd5bc66d540915c007bc
Sergey Morgun <sergey.morgun@rosbank.ru>
2024-03-17 23:03:08 +0500

    fix warnings

--- src/main.jsx
+++ src/main.jsx
@@ -73,79 +73,91 @@ root.render(
     React.createElement('main', {
         children: [
             React.createElement('section', {
+                key: restaurants[0].id,
                 children: [
-                    React.createElement('h2', { children: restaurants[0].name }),
-                    React.createElement('h3', { children: 'Меню'}),
+                    React.createElement('h2', { key: restaurants[0].name, children: restaurants[0].name }),
+                    React.createElement('h3', { key: 'menu0', children: 'Меню'}),
                     React.createElement('ul', {
+                        key: 'menuList0',
                         children: [
-                            React.createElement('li', { children: restaurants[0].menu[0].name }),
-                            React.createElement('li', { children: restaurants[0].menu[1].name }),
-                            React.createElement('li', { children: restaurants[0].menu[2].name })
+                            React.createElement('li', { key: restaurants[0].menu[0].id, children: restaurants[0].menu[0].name }),
+                            React.createElement('li', { key: restaurants[0].menu[1].id, children: restaurants[0].menu[1].name }),
+                            React.createElement('li', { key: restaurants[0].menu[2].id, children: restaurants[0].menu[2].name })
                         ]
                     }),
-                    React.createElement('h3', { children: 'Отзывы' }),
+                    React.createElement('h3', { key: 'reviews0', children: 'Отзывы' }),
                     React.createElement('ul', {
+                        key: 'reviewsList0',
                         children: [
-                            React.createElement('li', { children: restaurants[0].reviews[0].text }),
-                            React.createElement('li', { children: restaurants[0].reviews[1].text })
+                            React.createElement('li', { key: restaurants[0].reviews[0].id, children: restaurants[0].reviews[0].text }),
+                            React.createElement('li', { key: restaurants[0].reviews[1].id, children: restaurants[0].reviews[1].text })
                         ]
                     })
                 ]
             }),
             React.createElement('section', {
+                key: restaurants[1].id,
                 children: [
-                    React.createElement('h2', { children: restaurants[1].name }),
-                    React.createElement('h3', { children: 'Меню' }),
+                    React.createElement('h2', { key: restaurants[1].name, children: restaurants[1].name }),
+                    React.createElement('h3', { key: 'menu1', children: 'Меню' }),
                     React.createElement('ul', {
+                        key: 'menuList1',
                         children: [
-                            React.createElement('li', { children: restaurants[1].menu[0].name }),
-                            React.createElement('li', { children: restaurants[1].menu[1].name })
+                            React.createElement('li', { key: restaurants[1].menu[0].id, children: restaurants[1].menu[0].name }),
+                            React.createElement('li', { key: restaurants[1].menu[1].id, children: restaurants[1].menu[1].name })
                         ]
                     }),
-                    React.createElement('h3', { children: 'Отзывы' }),
+                    React.createElement('h3', { key: 'reviews1', children: 'Отзывы' }),
                     React.createElement('ul', {
+                        key: 'reviewsList1',
                         children: [
-                            React.createElement('li', { children: restaurants[1].reviews[0].text }),
-                            React.createElement('li', { children: restaurants[1].reviews[1].text }),
-                            React.createElement('li', { children: restaurants[1].reviews[2].text })
+                            React.createElement('li', { key: restaurants[1].reviews[0].id, children: restaurants[1].reviews[0].text }),
+                            React.createElement('li', { key: restaurants[1].reviews[1].id, children: restaurants[1].reviews[1].text }),
+                            React.createElement('li', { key: restaurants[1].reviews[2].id, children: restaurants[1].reviews[2].text })
                         ]
                     })
                 ]
             }),
             React.createElement('section', {
+                key: restaurants[2].id,
                 children: [
-                    React.createElement('h2', { children: restaurants[2].name }),
-                    React.createElement('h3', { children: 'Меню' }),
+                    React.createElement('h2', { key: restaurants[2].name, children: restaurants[2].name }),
+                    React.createElement('h3', { key: 'menu2', children: 'Меню' }),
                     React.createElement('ul', {
+                        key: 'menuList2',
                         children: [
-                            React.createElement('li', { children: restaurants[2].menu[0].name }),
-                            React.createElement('li', { children: restaurants[2].menu[1].name }),
-                            React.createElement('li', { children: restaurants[2].menu[2].name })
+                            React.createElement('li', { key: restaurants[2].menu[0].id, children: restaurants[2].menu[0].name }),
+                            React.createElement('li', { key: restaurants[2].menu[1].id, children: restaurants[2].menu[1].name }),
+                            React.createElement('li', { key: restaurants[2].menu[2].id, children: restaurants[2].menu[2].name })
                         ]
                     }),
-                    React.createElement('h3', { children: 'Отзывы' }),
+                    React.createElement('h3', { key: 'reviews2', children: 'Отзывы' }),
                     React.createElement('ul', {
+                        key: 'reviewsList2',
                         children: [
-                            React.createElement('li', { children: restaurants[2].reviews[0].text })
+                            React.createElement('li', { key: restaurants[2].reviews[0].id, children: restaurants[2].reviews[0].text })
                         ]
                     })
                 ]
             }),
             React.createElement('section', {
+                key: restaurants[3].id,
                 children: [
-                    React.createElement('h2', { children: restaurants[3].name }),
-                    React.createElement('h3', { children: 'Меню' }),
+                    React.createElement('h2', { key: restaurants[3].name, children: restaurants[3].name }),
+                    React.createElement('h3', { key: 'menu3', children: 'Меню' }),
                     React.createElement('ul', {
+                        key: 'menuList3',
                         children: [
-                            React.createElement('li', { children: restaurants[3].menu[0].name }),
-                            React.createElement('li', { children: restaurants[3].menu[1].name })
+                            React.createElement('li', { key: restaurants[3].menu[0].id, children: restaurants[3].menu[0].name }),
+                            React.createElement('li', { key: restaurants[3].menu[1].id, children: restaurants[3].menu[1].name })
                         ]
                     }),
-                    React.createElement('h3', { children: 'Отзывы' }),
+                    React.createElement('h3', { key: 'reviews3', children: 'Отзывы' }),
                     React.createElement('ul', {
+                        key: 'reviewsList3',
                         children: [
-                            React.createElement('li', { children: restaurants[3].reviews[0].text }),
-                            React.createElement('li', { children: restaurants[3].reviews[1].text })
+                            React.createElement('li', { key: restaurants[3].reviews[0].id, children: restaurants[3].reviews[0].text }),
+                            React.createElement('li', { key: restaurants[3].reviews[1].id, children: restaurants[3].reviews[1].text })
                         ]
                     })
                 ]
